8c77d16ee8111be1ba477ba311d700f6697e72ca
crasmek <itaysen@gmail.com>
2021-01-24 12:22:53 +0200

    Rain Total till given params station, channel

--- MvcWeather/Controllers/ValuesController.cs
+++ MvcWeather/Controllers/ValuesController.cs
@@ -26,29 +26,78 @@ namespace MvcWeather.Controllers
             return new string[] { "value1", "value2" };
         }
 
+
+
+
+        //[Route("api/Discount/GetData/{CategoryCode}/{Gender}")]
+       
+        //public string Get(int StationID, int ChannelID)
+        //{
+        //    return "to be developed...";
+        //}
+
+        
         // GET api/values/5
-        public string Get(int id)
+        public string Get(string id)  // 59_1 is beer sheva       
+        //public string Get(int id)
         {
-            //string jsonData = ServerUtils.Http.GetLatest(Convert.ToString(43), Convert.ToString(20));
-            string jsonData = ServerUtils.Http.GetDaily(Convert.ToString(43), Convert.ToString(20));
+            string str = id;
+            string jsonData;
+            if (str.Length >= 3)
+            {
+                string StationID = (str.Split('_'))[0];
+                string ChannelID = (str.Split('_'))[1];
+
+                jsonData = ServerUtils.Http.GetRainTillToday(StationID, ChannelID);
+                //jsonData = ServerUtils.Http.GetThisMonthsRain(StationID, ChannelID);  // temprorary for test purposes, to have less data.....
+
+
+            }
+            else
+            {
+                //string jsonData = ServerUtils.Http.GetLatest(Convert.ToString(43), Convert.ToString(20));
+
+                jsonData = ServerUtils.Http.GetDaily(Convert.ToString(43), Convert.ToString(20));
+            }
             // return "value";
             // need to analize json data 
-          
+
 
             //return ServerUtils.Http.testWeather();
 
             //DataContractJsonSerializer
-            JavaScriptSerializer js=new JavaScriptSerializer();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            js.MaxJsonLength = 50000000;
 
             dynamic obj = js.Deserialize<dynamic>(jsonData);
+            decimal Total = 0.0M;
+
+            for (int i = 0; i < obj["data"].Length;i++ )
+            {
 
 
-            return jsonData;
+
+                int PlaceHolderRemoveMe = 0;
+                if (obj["data"][i]["channels"][0]["valid"] == true)
+                    if (obj["data"][i]["channels"][0]["status"] == 1)
+                        if (obj["data"][i]["channels"][0]["value"] > 0 && obj["data"][i]["channels"][0]["value"] < 1000)
+                            Total += obj["data"][i]["channels"][0]["value"];
+                            
+
+            }
+            return Total.ToString();
+
+                //return jsonData;
             //return "2021 value "+id.ToString(); // itay
 
             // 05-02-2021, now need to build class to retrieve weather data
-//System.Runtime.Serialization.Json.DataContractJsonSerializer        
+            //System.Runtime.Serialization.Json.DataContractJsonSerializer        
         }
+
+
+
+
+
 
         // POST api/values
         public void Post([FromBody]string value)
--- MvcWeather/ServerUtils/Http.cs
+++ MvcWeather/ServerUtils/Http.cs
@@ -30,17 +30,94 @@ namespace MvcWeather.ServerUtils
         static string testUrl="https://api.ims.gov.il/v1/envista/stations";
         static string heroku = "https://cors-anywhere.herokuapp.com/";
         //static string heroku = string.Empty; // for test purposes only
+
+
+
+
+        static string GetTommorowDate()
+        {
+            //var date = new Date();
+            DateTime Tommorow = DateTime.Today.AddDays(1);
+
+            //date.setDate(date.getDate() + 1)
+
+
+            //var dd = String(date.getDate()).padStart(2, '0');
+
+            var dd = Tommorow.Day.ToString();
+            if (dd.Length == 1)
+                dd = "0" + dd;
+
+            //var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var mm = Tommorow.Month.ToString();
+            if (mm.Length == 1)
+                mm = "0" + mm;
+
+            //var yyyy = date.getFullYear();
+            var yyyy = Tommorow.Year.ToString();
+            //Tommorow
+
+            var dateStrTommorow = yyyy + '/' + mm + '/' + dd;
+            return dateStrTommorow;
+        }
+
+        // need to calculate day string
+        // js versions:
+        static string GetStartDate()
+        {
+            //var date = new Date();
+            //var StartYear = date.getFullYear();
+            //var mm = date.getMonth();
+            //if (mm < 7)  // 7 is august
+            //    StartYear = StartYear - 1;  // b/c count is from previous auguest
+            //var startDate = StartYear + "/08/01";
+            //return startDate;
+
+
+
+            DateTime today = DateTime.Today;
+            var StartYear = today.Year;
+            var mm = today.Month;
+            if (mm < 8)  // in c# , 8 is august (count starts from 1, not like JS)
+                StartYear -= 1;
+
+            var startDate = StartYear.ToString() + "/08/01";
+
+
+            
+
+
+
+            return startDate;
+
+
+
+        }
+
+
         static string GetThisMonthsRainUrl(string stationId, string channelId)
         {
             string res = BaseUrl + "/" + stationId + "/data/" + channelId + "/monthly";
             return res;
 
         }
+        //    const RainTillTodayUrl = `${BaseUrl}/${stationId}/data/${channelId}?from=${GetStartDate()}&to=${GetTommorowDate()}`;
 
+        public static string GetThisMonthsRain(string stationId, string channelId)
+        {
+            return GetData(GetThisMonthsRainUrl(stationId,channelId));
+        }
 
 
         public static string GetRainTillToday(string ST_ID, string CH_ID)
         {
+            string url = BaseUrl+"/" + ST_ID + "/data/" + CH_ID + "?from=" + GetStartDate() + "&to=" + GetTommorowDate();
+            return GetData(url);
+        }
+
+        public static string GetRainDaily(string ST_ID, string CH_ID)
+        {
+            GetStartDate();
             string url = "https://api.ims.gov.il/v1/envista/stations/" + ST_ID + "/data/" + CH_ID + "/daily";
             return GetData(url);
         }
@@ -50,12 +127,16 @@ namespace MvcWeather.ServerUtils
         //https://api.ims.gov.il/v1/envista/stations/{%ST_ID%}/data/{%CH_ID%}/latest
         public static string GetLatest(string ST_ID, string CH_ID)
         {
+            GetStartDate(); // for test purposes
             string url = "https://api.ims.gov.il/v1/envista/stations/" + ST_ID + "/data/" + CH_ID + "/latest";
             return GetData(url);
         }
 
         public static string GetDaily(string ST_ID, string CH_ID)
         {
+            GetTommorowDate(); // for test purposes
+            GetStartDate(); // for test purposes
+
             string url = "https://api.ims.gov.il/v1/envista/stations/" + ST_ID + "/data/" + CH_ID + "/daily";
             return GetData(url);
         }
